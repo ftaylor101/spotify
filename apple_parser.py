ff9a698ef61a8b0e543616ff484e9e95255c30c8
ftaylor101 <45371775+ftaylor101@users.noreply.github.com>
2024-12-25 07:08:58 +0000

    code tidy

--- apple_parser.py
+++ apple_parser.py
@@ -3,33 +3,33 @@ import pandas as pd
 
 class AppleParser:
     COLUMNS = [
-        'Album Name', 
-        'Container Album Name', 
-        'Device OS Name', 
-        'Device OS Version', 
-        'Device Type', 
-        'End Position In Milliseconds', 
-        'End Reason Type', 
-        'Event End Timestamp', 
-        'Event Received Timestamp', 
-        'Event Start Timestamp', 
-        'Event Timestamp', 
-        'Event Type', 
-        'Feature Name', 
-        'IP City', 
-        'IP Country Code', 
-        'IP Latitude', 
-        'IP Longitude', 
-        'IP Network Type', 
-        'Media Duration In Milliseconds', 
-        'Media Type', 
-        'Milliseconds Since Play', 
-        'Play Duration Milliseconds', 
-        'Song Name', 
-        'Start Position In Milliseconds', 
+        'Album Name',
+        'Container Album Name',
+        'Device OS Name',
+        'Device OS Version',
+        'Device Type',
+        'End Position In Milliseconds',
+        'End Reason Type',
+        'Event End Timestamp',
+        'Event Received Timestamp',
+        'Event Start Timestamp',
+        'Event Timestamp',
+        'Event Type',
+        'Feature Name',
+        'IP City',
+        'IP Country Code',
+        'IP Latitude',
+        'IP Longitude',
+        'IP Network Type',
+        'Media Duration In Milliseconds',
+        'Media Type',
+        'Milliseconds Since Play',
+        'Play Duration Milliseconds',
+        'Song Name',
+        'Start Position In Milliseconds',
         'UTC Offset In Seconds'
     ]
-    
+
     def __init__(self, file_path):
         self.df = pd.read_csv(file_path)
         self.df = self.df[self.COLUMNS]
