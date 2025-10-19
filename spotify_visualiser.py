29ccc58f838fe9ba4e762dcc9720a3fa0b1c7604
dai5yl0u <42715349+dai5yl0u@users.noreply.github.com>
2025-10-19 17:27:40 +0000

    Removed line saving csv and fixed longest song value

--- spotify_visualiser.py
+++ spotify_visualiser.py
@@ -63,9 +63,14 @@ if df_created:
 
     st.write("### Listening Stats :headphones:")
     df_finished_songs = df_date_filtered[df_date_filtered["reason_end"] == "trackdone"].sort_values(by="ms_played")
-    df_finished_songs.to_csv('output.csv', index=False)
     shortest_song = df_finished_songs.iloc[0]["master_metadata_track_name"]
-    longest_song = df_finished_songs.iloc[-1]["master_metadata_track_name"]
+    # The longest 'song' is typically a podcast, which doesn't have a track name so appears as N/A, this should be resolved by the refactor...
+    # Currently just loops back through the longest items until it finds a song
+    x = -1
+    longest_song = df_finished_songs.iloc[x]["master_metadata_track_name"]
+    while pd.isna(longest_song):
+        x -= 1
+        longest_song = df_finished_songs.iloc[x]["master_metadata_track_name"]
     total_time = df_date_filtered["ms_played"].sum() // (1000*60)
     unique_count = df_date_filtered["master_metadata_track_name"].nunique()
     listening_stats = pd.DataFrame({
